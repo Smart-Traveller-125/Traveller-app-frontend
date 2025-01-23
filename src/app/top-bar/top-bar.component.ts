44b90290eb54c35cb42c95683078432d7851293c
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2025-01-23 21:46:27 +0530

    project

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css'
+})
+export class TopBarComponent {
+
+}
